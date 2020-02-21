4d1b6c7d9a1be36e555d3197c45e0292a30ba266
simon.sharp <sharp@gyselroth.com>
2020-02-21 14:29:50 +0100

    Fix dynamic component

--- projects/sso-dynamic/src/lib/sso-dynamic.module.ts
+++ projects/sso-dynamic/src/lib/sso-dynamic.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 import {SsoDynamicComponentDirective} from './directives/sso-dynamic-component/sso-dynamic-component.directive';
 import {SsoDynamicModuleComponentDirective} from './directives/sso-dynamic-module-component/sso-dynamic-module-component.directive';
 import {SsoDynamicComponent} from './components/sso-dynamic/sso-dynamic.component';
+import {CommonModule} from '@angular/common';
 
 const COMPONENTS = [
   SsoDynamicComponent,
@@ -15,6 +16,7 @@ const COMPONENTS = [
     ...COMPONENTS
   ],
   imports: [
+      CommonModule
   ],
   exports: [
     ...COMPONENTS
